f8fb82bad9c41cb74d1c3bd79060c54ebcf8fa07
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-09-28 13:34:59 +0300

    Part 2 of the assignment was done

--- Assignment1/main.swift
+++ Assignment1/main.swift
@@ -1,22 +1,22 @@
 import Foundation
 
-let testData = [35, 12, 43, 8, 51, 27, 19, 3, 47, 30]
+let testData = (1...10000).map { _ in Int.random(in: 1...10000)}
 
-func Gnome(numbers: inout [Int]) {
-    var position = 0
+//func Gnome(numbers: inout [Int]) {
+    //var position = 0
     
-    while position < numbers.count {
-        if position == 0 || numbers[position] >= numbers[position - 1] {
-            position += 1
-        }
-        else
-        {
-            numbers.swapAt(position, position - 1)
-            position -= 1
-        }
-    }
-}
+    //while position < numbers.count {
+        //if position == 0 || numbers[position] >= numbers[position - 1] {
+            //position += 1
+        //}
+        //else
+        //{
+            //numbers.swapAt(position, position - 1)
+            //position -= 1
+        //}
+    //}
+//}
 
-var sortedArray = testData
-Gnome(numbers: &sortedArray)
+var sortedArray = testData.sorted()
+//Gnome(numbers: &sortedArray)
 print(sortedArray)
